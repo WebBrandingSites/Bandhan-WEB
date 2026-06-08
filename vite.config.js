f4390832634d1de1d8862eaefdf78d0069cff645
favazmk12-creator <favazmk12@gmail.com>
2026-06-08 11:35:22 +0530

    Use relative base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/Bandhan/',
+  base: './',
   build: {
     rollupOptions: {
       input: {
